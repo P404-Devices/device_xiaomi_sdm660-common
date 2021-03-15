9634a8964585e9a8cf54fc3eb020d5d7b15fb8e7
OdSazib <odsazib@gmail.com>
2021-03-15 12:21:09 +0600

    sdm660-common: Build power stats and label it BatteryStatsService: Unable to load Power Hal or power.stats HAL hwservicemanager: getTransport: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest. hwservicemanager: getTransport: Cannot find entry android.hardware.power@1.0::IPower/default in either framework or device manifest.

--- sdm660.mk
+++ sdm660.mk
@@ -337,7 +337,8 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service \
-    android.hardware.power-impl
+    android.hardware.power-impl \
+    android.hardware.power.stats@1.0-service.mock
     
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/config/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
